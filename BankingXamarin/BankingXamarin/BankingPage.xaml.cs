bd8ecc5ef9897348ee99794facac17804b053440
Allan Pead <adpead@gmail.com>
2020-12-04 13:40:28 +0200

    Corrected UI

--- BankingXamarin/BankingXamarin/BankingPage.xaml.cs
+++ BankingXamarin/BankingXamarin/BankingPage.xaml.cs
@@ -66,7 +66,7 @@ namespace BankingXamarin
 
             try
             {
-                var reason = DepositReasonEntry.Text;
+                var reason = WithdrawReasonEntry.Text;
                 if (valid)
                 {
                     var trans = _account.WithdrawMoney(withdrawAmount, DateTime.Now, reason);
